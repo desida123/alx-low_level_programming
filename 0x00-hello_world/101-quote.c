6e945756636f928f8ffb324822a1daacd2251a7e
Remi-Adesida Ibukunoluwa <remiadesidaibukunoluwa@gmail.com>
2023-06-17 11:14:13 -0700

    101 quote c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,6 @@
-#include <stdio.h>
+{ #include <stdio.h>
 
-/**
- * main - Entry point.
- *
- * Return: Always 1 (success)
- */
-int main(void)
-{
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (1);
-}
+int main(void) { write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+
+return (1);
+} }
