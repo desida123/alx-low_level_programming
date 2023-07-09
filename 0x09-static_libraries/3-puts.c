ebc6a3cdb9dfb80f88c65b8d597da07e8c71b9f3
Remi-Adesida Ibukunoluwa <remiadesidaibukunoluwa@gmail.com>
2023-07-08 20:39:55 -0700

    Static Libraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string to stdout
+ * @str: input string
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
